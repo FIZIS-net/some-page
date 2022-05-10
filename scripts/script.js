87509e4911833bef2759fb33202852f7c200241d
FIZIS-net <pshift4180@gmail.com>
2022-05-10 22:08:40 +0300

    added styles, scripts and images
    changed the styles of project blocks, added project-title that appear when you hover the mouse over the block.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-var a = 7;
+$('a.project')
+    .mouseenter( function(event){
+        $(event.target.parentElement).children('.project-title').fadeIn(200);
+    })
+    .mouseleave(function(event){
+        $(event.target.parentElement).children('.project-title').fadeOut(200);
+    })
